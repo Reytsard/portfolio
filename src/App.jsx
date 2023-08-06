368a69b10bf907fe786204bdbd11a5d8c6e56a6e
Ronald De Mesa <ronaldreyjr061201@gmail.com>
2023-08-06 11:58:04 +0800

    Added Contact number on contact portion

--- src/App.jsx
+++ src/App.jsx
@@ -196,13 +196,16 @@ function App() {
           </div>
           <div id="contactme" className="bg-black w-100 text-white py-3">
             <h3 className="contact-me-header ">Contact Me</h3>
-            Email:{" "}
-            <a
-              href="emailto:rrey.dm@gmail.com"
-              className="text-decoration-none text-white curser-pointer"
-            >
-              Rrey.dm@gmail.com
-            </a>
+            <div>
+              Email:{" "}
+              <a
+                href="emailto:rrey.dm@gmail.com"
+                className="text-decoration-none text-white curser-pointer"
+              >
+                Rrey.dm@gmail.com
+              </a>
+            </div>
+            <div>Contact Number: +63 981 322 9906</div>
           </div>
         </div>
       </main>
