0612c690c854ac3086af48d20e9fbe2531e1b3d0
Ronald De Mesa <ronaldreyjr061201@gmail.com>
2023-08-12 13:50:38 +0800

    Added description on tech stack skill

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import "./styles/main.css";
 import Image1 from "./images/1.png";
+import Pfp from "./images/1 (copy).png";
 import Image2 from "./images/proj1.png";
 import Image3 from "./images/proj2.png";
 import Image4 from "./images/proj3.png";
@@ -17,6 +18,7 @@ import Image14 from "./images/next-js.png";
 import Image15 from "./images/Bootstrap.png";
 import Image16 from "./images/online-shop.png";
 import Image17 from "./images/brewery-locator.png";
+import Image18 from "./images/react-router-stacked-color.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHand } from "@fortawesome/free-solid-svg-icons";
 
@@ -41,49 +43,187 @@ function App() {
         <div className="home">
           <div className="greetings d-flex flex-wrap gap-4 justify-content-center align-items-center">
             <div className="imageContainer">
-              <img
+              {/* <img
                 className="greetingsImg rounded-circle shadow-lg"
                 src={Image1}
                 alt="Person in a suit"
                 height="200px"
-              />
+              /> */}
+              <svg
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 500 500"
+                width="100%"
+                id="blobSvg"
+                style={{ opacity: "1" }}
+                transform="rotate(0)"
+                filter="blur(0px)"
+              >
+                <image
+                  x="0"
+                  y="40"
+                  width="100%"
+                  height="100%"
+                  clip-path="url(#shape)"
+                  href={Image1}
+                  preserveAspectRatio="none"
+                ></image>{" "}
+                <defs>
+                  {" "}
+                  <linearGradient
+                    id="gradient"
+                    x1="0%"
+                    y1="0%"
+                    x2="0%"
+                    y2="100%"
+                  >
+                    {" "}
+                    <stop
+                      offset="0%"
+                      style={{ "stop-color": "rgb(238, 205, 163)" }}
+                    ></stop>{" "}
+                    <stop
+                      offset="100%"
+                      style={{ "stop-color": "rgb(239, 98, 159)" }}
+                    ></stop>{" "}
+                  </linearGradient>{" "}
+                </defs>{" "}
+                <clipPath id="shape">
+                  <path id="blob" fill="url(#gradient)">
+                    {" "}
+                    <animate
+                      attributeName="d"
+                      dur="10000ms"
+                      repeatCount="indefinite"
+                      values="M440.5,320.5Q418,391,355.5,442.5Q293,494,226,450.5Q159,407,99,367Q39,327,31.5,247.5Q24,168,89,125.5Q154,83,219.5,68Q285,53,335.5,94.5Q386,136,424.5,193Q463,250,440.5,320.5Z;M453.78747,319.98894Q416.97789,389.97789,353.96683,436.87838Q290.95577,483.77887,223.95577,447.43366Q156.95577,411.08845,105.64373,365.97789Q54.33169,320.86732,62.67444,252.61056Q71.01719,184.3538,113.01965,135.21007Q155.02211,86.06634,220.52211,66.46683Q286.02211,46.86732,335.5,91.94472Q384.97789,137.02211,437.78747,193.51106Q490.59704,250,453.78747,319.98894Z;M411.39826,313.90633Q402.59677,377.81265,342.92059,407.63957Q283.24442,437.46649,215.13648,432.5428Q147.02853,427.61911,82.23325,380.9572Q17.43796,334.29529,20.45223,250.83809Q23.46649,167.38089,82.5856,115.05707Q141.70471,62.73325,212.19045,63.73015Q282.67618,64.72705,352.67308,84.79839Q422.66998,104.86972,421.43486,177.43486Q420.19974,250,411.39826,313.90633Z;M440.5,320.5Q418,391,355.5,442.5Q293,494,226,450.5Q159,407,99,367Q39,327,31.5,247.5Q24,168,89,125.5Q154,83,219.5,68Q285,53,335.5,94.5Q386,136,424.5,193Q463,250,440.5,320.5Z;"
+                    ></animate>{" "}
+                  </path>
+                </clipPath>
+              </svg>
             </div>
             <div className="words">
               <span>
                 Hi!{" "}
                 <FontAwesomeIcon icon={faHand} style={{ color: "#000000" }} />
               </span>
-              <span>I am Ronald De Mesa</span>
-              <span>Web Developer</span>
+              <h1>I am Ronald De Mesa</h1>
+              <h2>Web Developer</h2>
             </div>
           </div>
-          <div id="skills">
-            <div className="skillDiv">
-              <img className="skill-image" src={Image8} alt="html" />
+          <div
+            id="skills"
+            className="fs-5 d-flex flex-column align-items-center"
+          >
+            <div className="skill d-flex flex-wrap h-auto w-auto mx-3 p-1 justify-content-center">
+              <div className="skillDiv d-flex align-items-center gap-3 m-2">
+                <img src={Image14} alt="nextjs" className="skill-image" />
+                <div className="border d-flex m-2 px-2 rounded-2 shadow-lg">
+                  Vercel
+                </div>
+              </div>
+              Website rendering and deploying
             </div>
-            <div className="skillDiv">
-              <img className="skill-image" src={Image9} alt="css" />
+            <div className="skill d-flex flex-wrap h-auto w-auto mx-3 p-1 justify-content-center">
+              <div className="skillDiv d-flex gap-3 m-2">
+                <img className="skill-image" src={Image11} alt="react" />
+              </div>
+              Development of multi-page applications and app optimization
+              (React)
             </div>
-            <div className="skillDiv">
-              <img className="skill-image" src={Image10} alt="js" />
+            <div className="skill d-flex flex-wrap h-auto w-auto mx-3 p-1 justify-content-center">
+              <div className="skillDiv d-flex gap-3 m-2">
+                <div className="skillDiv">
+                  <img className="skill-image" src={Image12} alt="Redux" />
+                </div>
+              </div>
+              Data management and integration of Google Maps (Redux)
             </div>
-            <div className="skillDiv">
-              <img className="skill-image" src={Image11} alt="react" />
+            <div className="skill d-flex flex-wrap h-auto w-auto mx-3 p-1 justify-content-center">
+              <img
+                className="skill-image mx-2"
+                src={Image18}
+                alt="React-Router"
+              />
+              Routing, navigation, and forms development
             </div>
-            <div className="skillDiv">
-              <img className="skill-image" src={Image12} alt="sass" />
+            <div className="skill d-flex flex-wrap h-auto w-auto mx-3 p-1 justify-content-center">
+              <div className="skillDiv d-flex gap-3 m-2">
+                <img className="skill-image" src={Image8} alt="html" />
+                <img className="skill-image" src={Image9} alt="css" />
+                <img className="skill-image" src={Image13} alt="Sass" />
+                <img className="skill-image" src={Image15} alt="bootstrap" />
+              </div>
+              Adaptive and responsive website layout and styling (HTML, CSS,
+              SASS)
             </div>
-            <div className="skillDiv">
-              <img className="skill-image" src={Image13} alt="Redux" />
+            <div className="skill d-flex flex-wrap h-auto w-auto mx-3 p-1 justify-content-center">
+              <div className="skillDiv d-flex gap-3 m-2">
+                <img className="skill-image" src={Image10} alt="js" />
+              </div>
+              Development of function and manipulation of strings and arrays
+              (JavaScript)
             </div>
-            <div className="skillDiv">
-              <img src={Image14} alt="nextjs" className="skill-image" />
+            <div className="skill d-flex flex-wrap h-auto w-auto mx-3 p-1 justify-content-center">
+              <div className="border d-flex gap-3 m-2 px-2 rounded-2 shadow-lg">
+                DOM
+              </div>
+              Connection of functions in JavaScript and an HTML layout
             </div>
-            <div className="skillDiv">
-              <img className="skill-image" src={Image15} alt="bootstrap" />
+            <div className="skill d-flex flex-wrap h-auto w-auto mx-3 p-1 justify-content-center">
+              <div className="border d-flex gap-3 m-2 px-2 rounded-2 shadow-lg">
+                HTTP
+              </div>
+              <div className="border d-flex gap-3 m-2 px-2 rounded-2 shadow-lg">
+                API
+              </div>
+              <div className="border d-flex gap-3 m-2 px-2 rounded-2 shadow-lg">
+                Axios
+              </div>
+              Connection of server and client data
             </div>
-
-            <div className="skill">Axios</div>
+            <div className="skill d-flex flex-wrap h-auto w-auto mx-3 p-1 justify-content-center">
+              <div className="border d-flex gap-3 m-2 px-2 rounded-2 shadow-lg">
+                NPM
+              </div>
+              Libraries installation and management
+            </div>
+            <div className="skill d-flex flex-wrap h-auto w-auto mx-3 p-1 justify-content-center">
+              <div className="skillDiv d-flex gap-3 m-2">
+                <div className="border d-flex gap-3 m-2 px-2 rounded-2 shadow-lg">
+                  Unit Testing
+                </div>
+                <div className="border d-flex gap-3 m-2 px-2 rounded-2 shadow-lg">
+                  React Testing
+                </div>
+              </div>
+              Code Testing and bug fixing
+            </div>
+            <div className="skill d-flex flex-wrap h-auto w-auto mx-3 p-1 justify-content-center">
+              <div className="skillDiv d-flex flex-wrap justify-content-around gap-3 m-2">
+                <div className="border d-flex gap-3 m-2 px-2 rounded-2 shadow-lg">
+                  Git
+                </div>
+                <div className="border d-flex gap-3 m-2 px-2 rounded-2 shadow-lg">
+                  GitHub
+                </div>
+                <div className="border d-flex gap-3 m-2 px-2 rounded-2 shadow-lg">
+                  SemVer
+                </div>
+                <div className="border d-flex gap-3 m-2 px-2 rounded-2 shadow-lg">
+                  Gitflow
+                </div>
+              </div>
+              Product versions management, collaboration and pull requests, and
+              portfolio creation
+            </div>
+            <div className="skill d-flex flex-wrap h-auto w-auto mx-3 p-1 justify-content-center">
+              <div className="skillDiv d-flex gap-3 m-2">
+                <div className="border d-flex gap-3 m-2 px-2 rounded-2 shadow-lg">
+                  Auth0
+                </div>
+              </div>
+              Data safety through authentication and authorization
+            </div>
           </div>
           <div id="about">
             <h2>About</h2>
@@ -91,10 +231,10 @@ function App() {
             <span>Address: Baguio City</span>
             <span>Country: Philippines</span>
           </div>
-          <div id="projects">
+          <div id="projects" className="p-1">
             <div id="title">Sample Projects</div>
             <div className="projects">
-              <div className="card d-flex flex-column justify-content-evenly">
+              <div className="card d-flex w-auto flex-column justify-content-evenly">
                 <a
                   href="https://online-shop-three-lac.vercel.app/"
                   target="_blank"
@@ -230,3 +370,49 @@ function App() {
 }
 
 export default App;
+
+{
+  /* <div className="card">
+<a
+  href="http://reytsard.github.io/news-website-refocus"
+  target="_blank"
+  rel="noreferrer"
+>
+  <img src={Image6} alt="online-clothing-store" width="320px" />
+</a>
+<div className="skills d-flex justify-content-center gap-2 flex-wrap">
+  <div className="card mx-1 px-1">Javascript</div>
+  <div className="card mx-1 px-1">HTML</div>
+  <div className="card mx-1 px-1">CSS</div>
+</div>
+</div>
+<div className="card">
+<a
+  href="http://reytsard.github.io/news-website-refocus"
+  target="_blank"
+  rel="noreferrer"
+>
+  <img src={Image6} alt="project-5" width="320px" />
+</a>
+<div className="skills d-flex justify-content-center gap-2 flex-wrap">
+  <div className="card mx-1 px-1">Javascript</div>
+  <div className="card mx-1 px-1">HTML</div>
+  <div className="card mx-1 px-1">CSS</div>
+</div>
+</div>
+<div className="card">
+<a
+  href="http://reytsard.github.io/resume-of-mine"
+  target="_blank"
+  rel="noreferrer"
+>
+  <img src={Image5} alt="adaptive-resume" width="320px" />
+   change Image if saved already 
+</a>
+<div className="skills d-flex justify-content-center gap-2 flex-wrap">
+  <div className="card mx-1 px-1">Responsive Design</div>
+  <div className="card mx-1 px-1">HTML</div>
+  <div className="card mx-1 px-1">CSS</div>
+</div>
+</div> */
+}
